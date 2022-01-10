60f5e7ff17dab410bc4ab8e3cc5a4a1966c16ec3
me6edi <exited17@gmail.com>
2022-01-10 22:39:47 +0600

    Rendering data to template

--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import *
 
 # Create your views here.
 
 def home(request):
-    return render(request,'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    context = {'orders':orders,'customers':customers}
+
+    return render(request,'accounts/dashboard.html', context)
 
 def products(request):
-    return render(request,'accounts/products.html')
+    products = Product.objects.all()
+    return render(request,'accounts/products.html',{'products':products})
 
 def customer(request):
     return render(request,'accounts/customer.html')
