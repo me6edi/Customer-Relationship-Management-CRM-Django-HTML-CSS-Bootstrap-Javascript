f8388c772795a2f291293f088867eef6ef95ce7d
me6edi <exited17@gmail.com>
2022-01-24 22:50:01 +0600

    complite CRUD section

--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -1,5 +1,5 @@
 from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import *
 from .forms import OrderForm
@@ -35,8 +35,35 @@ def customer(request, pk_test):
 
 def createOrder(request):
 
-    form  = OrderForm
-    
-    context = {'form':form}
+    form  = OrderForm()
+    if request.method == 'POST':
+        #print('Printing POST:', request.POST)
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
 
+    context = {'form':form}
     return render(request, 'accounts/order_form.html', context)
+
+def updateOrder(request,pk):
+
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form':form}
+    return render(request, 'accounts/order_form.html', context)
+
+def deleteOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    if request.method == "POST":
+        order.delete()
+        return redirect('/')
+    context = {'item':order}
+    return render(request, 'accounts/delete.html', context)
