93ec15d1fb32cc6179a4ef3095bd34ba48509552
me6edi <exited17@gmail.com>
2022-01-31 19:10:06 +0600

    Filter Form TAble Search

--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from .models import *
 from .forms import OrderForm
+from .filters import OrderFilter
 
 # Create your views here.
 
@@ -30,7 +31,10 @@ def customer(request, pk_test):
     orders = customer.order_set.all()
     order_count = orders.count()
 
-    context = {'customer':customer, 'orders':orders, 'order_count':order_count}
+    myFilter = OrderFilter(request.GET, queryset=orders)
+    orders = myFilter.qs
+
+    context = {'customer':customer, 'orders':orders, 'order_count':order_count,'myFilter':myFilter}
     return render(request,'accounts/customer.html',context)
 
 def createOrder(request, pk):
