2d2ac1cde1cdd9a7e9b5bf18734cf2780ec45b69
me6edi <exited17@gmail.com>
2022-02-14 21:39:54 +0600

    Image File Upload to User Profile Model

--- crm1/accounts/forms.py
+++ crm1/accounts/forms.py
@@ -1,13 +1,19 @@
 from dataclasses import field
 from pyexpat import model
+from statistics import mode
 from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
-from .models import Order
+from .models import Customer, Order
 
+class CustomerForm(ModelForm):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+        exclude = ['user']
 class OrderForm(ModelForm):
     class Meta:
         model = Order
--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -16,7 +16,7 @@ from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 from .models import *
-from .forms import OrderForm,CreateUserForm
+from .forms import OrderForm, CreateUserForm, CustomerForm
 from .filters import OrderFilter
 from .decorators import unauthenticated_user, allowed_users, admin_only
 
@@ -96,6 +96,22 @@ def userPage(request):
 
     context = {'orders':orders,'total_orders':total_orders,'delivered':delivered,'pending':pending}
     return render(request, 'accounts/user.html', context)
+
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
+def accountSettings(request):
+	customer = request.user.customer
+	form = CustomerForm(instance=customer)
+
+	if request.method == 'POST':
+		form = CustomerForm(request.POST, request.FILES,instance=customer)
+		if form.is_valid():
+			form.save()
+
+
+	context = {'form':form}
+	return render(request, 'accounts/account_settings.html', context)
 
 
 @login_required(login_url='login')
